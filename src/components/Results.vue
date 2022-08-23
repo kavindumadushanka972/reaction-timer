7eee2410384c58993e61baad9891261037d101b2
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2022-08-23 15:11:02 +0530

    start and end game with results

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="block">
+        <p>Reaction time: {{ score }} ms</p>
+
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Flash Fingers :-O"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes :)"
+        } else {
+            this.rank = "Snail pace :("
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: black;
+        font-weight: bold;
+    }
+</style>
